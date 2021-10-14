0571496cfe0383815bd35fbc69912fcbb44efc99
Anderson Assunção da Fonseca <36905253+andersnfonseca@users.noreply.github.com>
2021-10-14 16:43:36 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,46 @@
+'use strict'
+
+var horas = 0;
+var minutos = 0;
+var segundos = 0;
+
+var tempo = 1000; 
+var cronometro; 
+
+function start() {
+    cronometro = setInterval(() => { timer(); }, tempo);
+}
+
+function pause() {
+    clearInterval(cronometro);
+}
+
+function stop() {
+    clearInterval(cronometro);
+    horas = 0; 
+    minutos = 0;
+    segundos = 0;
+
+    document.getElementById("contador").innerText = "00:00:00";
+}
+
+function timer() {
+    segundos++;
+
+    if (segundos == 60) {
+        segundos = 0
+        minutos++
+    
+    if (minutos == 60) {
+        minutos = 0;
+        horas++
+    }
+}
+
+var formato = (horas < 10 ? '0' + horas: horas) + ':' + (minutos < 10 ? '0' + minutos : minutos) + ':' + (segundos < 10 ? '0' + segundos : segundos);
+
+document.getElementById("contador").innerText = formato;
+
+return formato;      
+
+}
